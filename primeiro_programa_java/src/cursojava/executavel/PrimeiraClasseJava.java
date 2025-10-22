ba1b9d74a5783acfa49069722ee6aebcb927091f
tecnologiagmi <tecnologia.seguranca@itaborai.rj.gov.br>
2025-10-22 12:52:15 -0300

    Criação do Projeto

---
+++ primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/* Main é um método auto executável em JAVA */
+	public static void main(String[] args) {
+		
+		/* Escreve texto no Console */
+		System.out.println("Executável");
+		
+	}
+
+}
