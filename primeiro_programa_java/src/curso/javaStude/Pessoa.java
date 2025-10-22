a0ef4d3cf001e7feddb56eba21726e09d742a88a
tecnologiagmi <tecnologia.seguranca@itaborai.rj.gov.br>
2025-10-22 15:12:34 -0300

    Adicionando o campo de Idade na Classe Pessoa

--- primeiro_programa_java/src/curso/javaStude/Pessoa.java
+++ primeiro_programa_java/src/curso/javaStude/Pessoa.java
@@ -10,11 +10,20 @@ public class Pessoa {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	private int idade;
+	
 	public String getSobrenome() {
 		return sobrenome;
 	}
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	
 
